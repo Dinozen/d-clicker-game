cdab711b760a23b19538e20f155c2216189d8270
Dinozen <mercimektek@gmail.com>
2024-07-22 02:53:29 +0300

    Add files via upload

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebhookInfo
 from telegram.ext import Application, CommandHandler, CallbackContext
-import os
 
 API_KEY = '7232543351:AAEIEL74ZB6Zpls30bbMamnd_-G-w7OQigk'
 WEB_APP_URL = 'https://dinozen.github.io/d-clicker-game/'
@@ -8,7 +7,8 @@ WEB_APP_URL = 'https://dinozen.github.io/d-clicker-game/'
 application = Application.builder().token(API_KEY).build()
 
 async def start(update: Update, context: CallbackContext):
-    keyboard = [[InlineKeyboardButton("🚀 Launch Game 🚀", web_app=WebAppInfo(url=WEB_APP_URL))]]
+    # WebhookInfo yerine URL'yi doğrudan kullanmak zorunda kalıyoruz
+    keyboard = [[InlineKeyboardButton("🚀 Launch Game 🚀", url=WEB_APP_URL)]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text(
         "Get ready to dive into the dino-tastic world of Dino Farm! Click the button below to start farming your $DINOZ fortune! 🦖💰",
